ba065c142bbaf7a673d1cac9a221254778b99207
Cedric von Allmen <cnvonallmen@hotmail.com>
2017-12-04 19:19:04 +0100

    added first version of naive bayes algorithm

---
+++ src/main/java/ch/bfh/bti7535/w2017/databoys69/algorithm/DataboysNaiveBayes.java
@@ -0,0 +1,71 @@
+package ch.bfh.bti7535.w2017.databoys69.algorithm;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class DataboysNaiveBayes implements Runnable {
+
+    private File trainingData;
+
+    private File testData;
+
+    /**
+     * Creates an Instance of a @{@link DataboysNaiveBayes} algorithm
+     * @param trainingData the training data file in the <code>.arff</code> format
+     * @param testData the test data file in the <code>.arff</code> format
+     */
+    public DataboysNaiveBayes(File trainingData, File testData) {
+        CheckExistsAndNotNull(trainingData);
+        CheckExistsAndNotNull(testData);
+
+        this.trainingData = trainingData;
+        this.testData = testData;
+    }
+
+    /**
+     * Runs Databoys Naive Bayes algorithm
+     */
+    public void run() {
+        try {
+            //filter
+            StringToWordVector filter = new StringToWordVector();
+            Classifier naive = new NaiveBayes();
+
+            //training data
+            Instances train = new Instances(new BufferedReader(new FileReader(trainingData)));
+            int lastIndex = train.numAttributes() - 1;
+            train.setClassIndex(lastIndex);
+            filter.setInputFormat(train);
+            train = Filter.useFilter(train, filter);
+
+            //testing data
+            Instances test = new Instances(new BufferedReader(new FileReader(testData)));
+            test.setClassIndex(lastIndex);
+            filter.setInputFormat(test);
+            Instances test2 = Filter.useFilter(test, filter);
+
+            naive.buildClassifier(train);
+            for(int i=0; i<test2.numInstances(); i++) {
+                System.out.println(test.instance(i));
+                double index = naive.classifyInstance(test2.instance(i));
+                String className = train.attribute(0).value((int)index);
+                System.out.println(className);
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void CheckExistsAndNotNull(File file) {
+        if (file == null || !file.exists()) {
+            throw new IllegalArgumentException("File must exists and must not be null");
+        }
+    }
+}
